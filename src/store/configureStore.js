2e5fd23926995088805cb4c079b5612888b5c8c8
rimiti <dimitri.dobairro@dimsolution.com>
2018-01-05 10:52:08 +0100

    feat(store/configureStore.js) since 3.0.0 redux-logger exports by default logger with default settings

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import createLogger from 'redux-logger'
+import {createLogger} from 'redux-logger'
 // import thunkMiddleware from 'redux-thunk'
 // import devTools from 'remote-redux-devtools'
 import { createStore, applyMiddleware, compose } from 'redux'
